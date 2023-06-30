61ffe322af713b42200f1f40da4bcdf39712caf5
Vinicius Bastos <vclemente2@hotmail.com>
2023-06-29 21:16:32 -0300

    Implementação dor eset.css e estilazações globais do projeto.

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,13 @@
 import { Button } from "../../components/Button";
 import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "../../assets/images/logo.svg";
 
 export function Cardapio() {
   return (
     <>
-      <nav className={styles.menu}>hello world</nav>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
       <Button>Teste</Button>
     </>
   );
