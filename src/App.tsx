a8a910d591cfdc77152a8f22e32732722c64eb16
Vinicius Bastos <vclemente2@hotmail.com>
2023-07-02 11:42:13 -0300

    Implementação de rotas com a utilização da biblioteca react-router-dom.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Cardapio } from "./pages/Cardapio";
 import { Inicio } from "./pages/Inicio";
 
 function App() {
   return (
-    <main>
-      <Inicio />
-    </main>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/cardapio" element={<Cardapio />} />
+      </Routes>
+    </Router>
   );
 }
 
